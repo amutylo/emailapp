5535a300183bc9226c4b9394ce26b72f7c4998af
Andrii Mutylo <amutylo@webcodin.com>
2020-02-04 12:47:29 +0200

    load messages from external json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'MessageList.dart';
+
 void main() => runApp(EmailApp());
 
 class EmailApp extends StatelessWidget {
@@ -8,46 +10,8 @@ class EmailApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blueGrey, accentColor: Colors.red),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MessageList(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  var messages = const [
-    'First message',
-    'Second message',
-    'Second one',
-    'You won this game',
-    'You won that game',
-    'You have to read this!',
-    'You won this game',
-  ];
-  MyHomePage({this.title});
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-      ),
-      body: ListView.separated(
-        itemCount: 10,
-        separatorBuilder: (context, index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          var title = messages[index];
-          return ListTile(
-            title: Text('Title $index'),
-            isThreeLine: true,
-            leading: CircleAvatar(
-              child: Text('Pj'),
-            ),
-            subtitle: Text('Another text very long one.'),
-          );
-        },
-      )
-    );
-  }
-}
