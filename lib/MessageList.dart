c2f49429885b8707310dbaf30d12b374205f2c47
Andrii Mutylo <amutylo@webcodin.com>
2020-02-07 12:48:24 +0200

    Slid actions implemented on each element from the list.

--- lib/MessageList.dart
+++ lib/MessageList.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'ComposeButton.dart';
 import 'Message.dart';
@@ -36,9 +37,8 @@ class _MessageListState extends State<MessageList> {
         title: Text(widget.title),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.refresh), onPressed: () async {
-            var _messages = await Message.browse();
             setState(() {
-              messages = _messages;
+              future = Message.browse();
             });
           },)
         ],
@@ -125,24 +125,61 @@ class _MessageListState extends State<MessageList> {
               separatorBuilder: (context, index) => Divider(),
               itemBuilder: (BuildContext context, int index) {
                 Message message = messages[index];
-                return ListTile(
-                  title: Text(message.subject),
-                  isThreeLine: true,
-                  leading: CircleAvatar(
-                    child: Text('Pa'),
+                return Slidable(
+                  actionPane: SlidableDrawerActionPane(),
+                  actionExtentRatio: 0.25,
+                  actions: <Widget>[
+                    IconSlideAction(
+                      caption: 'Archive',
+                      color: Colors.blue,
+                      icon: Icons.archive,
+                      onTap: () {}
+                    ),
+                    IconSlideAction(
+                      caption: 'Share',
+                      color: Colors.blue,
+                      icon: Icons.share,
+                      onTap: () {}
+                    ),
+                  ],
+                  secondaryActions: <Widget>[
+                    IconSlideAction(
+                      caption: 'More',
+                      color: Colors.blue,
+                      icon: Icons.more,
+                      onTap: () {}
+                    ),
+                    IconSlideAction(
+                      caption: 'Delete',
+                      color: Colors.blue,
+                      icon: Icons.delete,
+                      onTap: () {
+                        setState(() {
+                          messages.removeAt(index);
+                        });
+                      }
+                    ),
+                  ],
+                  child: ListTile(
+                    title: Text(message.subject),
+                    isThreeLine: true,
+                    leading: CircleAvatar(
+                      child: Text('Pa'),
+                    ),
+                    subtitle: Text(
+                      message.body,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                          MessageDetail(message.subject, message.body)
+                      ));
+                    }
                   ),
-                  subtitle: Text(
-                    message.body,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                        MessageDetail(message.subject, message.body)
-                    ));
-                  }
+                  key: ObjectKey(message),
                 );
               },
             );
