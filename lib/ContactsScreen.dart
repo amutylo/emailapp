cbba9fbff69b5478dd2909d1fb805979986da537
Andrii Mutylo <amutylo@webcodin.com>
2020-02-12 15:33:46 +0200

    Added contacts page as Stream

--- lib/ContactsScreen.dart
+++ lib/ContactsScreen.dart
@@ -1,8 +1,9 @@
 import 'package:emailapp/AppDrawer.dart';
+import 'package:emailapp/ContactManager.dart';
 import 'package:flutter/material.dart';
 
 class ContactsScreen extends StatelessWidget {
-
+  ContactManager manager = ContactManager();
   @override
 
   Widget build (BuildContext context) {
@@ -10,10 +11,43 @@ class ContactsScreen extends StatelessWidget {
     return Scaffold(
           appBar: AppBar(
             title: Text("Contacts"),
+            actions: <Widget>[
+              Chip(
+                label: StreamBuilder<int>(
+                  stream: manager.contactCounter,
+                  builder: (context, snapshot) {
+                    return Text(
+                      (snapshot.data?? 0).toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      ),
+                    );
+                  }
+                ),
+                backgroundColor: Colors.red,
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 16)
+              )
+            ]
           ),
           drawer: AppDrawer(),
-          body: Center(
-            child: Text("Contacts")
+          body: StreamBuilder<List<String>>(
+            stream: manager.contactListNow,
+            builder: (context, snapshot) {
+              List<String> contacts = snapshot.data;
+              int contactsLength = (contacts != null) ? contacts.length : 0;
+              return ListView.separated(
+                itemCount: contactsLength,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(contacts[index]),
+                  );
+                },
+                separatorBuilder: (context, index) => Divider(),
+              );
+            }
           ),
     );
   }
